149f94534cb506a532b524dc8140ec278d75065b
NGUYEN Alexandre <alex.nr@hotmail.fr>
2014-11-14 23:26:09 +0100

    Tests unmarshal json

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -4,6 +4,9 @@ import "github.com/revel/revel"
 import "database/sql"
 import _ "github.com/go-sql-driver/mysql"
 import "fmt"
+import "os"
+import "encoding/json"
+import "io/ioutil"
 
 type App struct {
 	*revel.Controller
@@ -65,10 +68,53 @@ func (c App) Index() revel.Result {
 	greeting := "test json"
 
 
-    var arr = []Example {
-        {{"a", "b", "c"}},
+    type jsonobject struct {
+    Object ObjectType
+}
+ 
+type ObjectType struct {
+    Buffer_size int
+    Databases   []DatabasesType
+}
+ 
+type DatabasesType struct {
+    Host   string
+    User   string
+    Pass   string
+    Type   string
+    Name   string
+    Tables []TablesType
+}
+ 
+type TablesType struct {
+    Name     string
+    Statment string
+    Regex    string
+    Types    []TypesType
+}
+ 
+type TypesType struct {
+    Id    string
+    Value string
+}
+ 
+// Main function
+// I realize this function is much too simple I am simply at a loss to
+ 
+func main() {
+    file, e := ioutil.ReadFile("./config.json")
+    if e != nil {
+        fmt.Printf("File error: %v\n", e)
+        os.Exit(1)
     }
-    fmt.Println(arr)    
+    fmt.Printf("%s\n", string(file))
+ 
+    //m := new(Dispatch)
+    //var m interface{}
+    var jsontype jsonobject
+    json.Unmarshal(file, &jsontype)
+    fmt.Printf("Results: %v\n", jsontype)
+} 
 
 
 	return c.Render(greeting)
