020e0ee34307526dea498073fb2f3ec958c52c28
NGUYEN Alexandre <alex.nr@hotmail.fr>
2014-11-19 22:35:45 +0100

    Changing name policy

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -123,7 +123,7 @@ func main() {
 func (c App) Hello(myName string) revel.Result {
 	
 	c.Validation.Required(myName).Message("Your name is required!")
-	c.Validation.MinSize(myName, 3).Message("Your name is not long enough!")
+	c.Validation.MinSize(myName, 10).Message("Your name is not long enough!")
 
 	if c.Validation.HasErrors() {
 		c.Validation.Keep()
